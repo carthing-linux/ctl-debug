0cdb3ced07b9cfe7c674b964fc20fb94b43ee0c6
Charlie Sale <softwaresale01@gmail.com>
2024-11-03 21:24:10 -0500

    Initial commit

---
+++ build.rs
@@ -0,0 +1,4 @@
+
+fn main() {
+    slint_build::compile("ui/main.slint").expect("main.slint failed to compile");
+}
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+
+slint::include_modules!();
+
+fn main() -> Result<(), slint::PlatformError> {
+    
+    let main_window = MainWindow::new()?;
+
+    main_window.run()?;
+
+    Ok(())
+}
